0f45f38dcae7725531629af37157434579e5a415
Jen <jen.johnson7@icloud.com>
2017-03-10 22:09:34 -0500

    puts all nouns from corpus text into json file as nested dict

--- test.py
+++ test.py
@@ -1,52 +1,92 @@
 import sys
-from en import verb
-# MAKE THIS FASTERRRRRR!!!!!!!!!
-from nltk.corpus import brown, gutenberg
-from nltk import sent_tokenize, word_tokenize, pos_tag
+import json
+from en import verb, noun
+from nltk.corpus import brown
+#from nltk.corpus import gutenberg
+from nltk import sent_tokenize
+from nltk import pos_tag
 
-#get sentences from a single text stored as a list
-sentence_list = gutenberg.sents('austen-emma.txt')
-filtered_list = []
+def find_next_verb(noun_index, sentence, singular_noun, all_nouns):
+    current_pos = "NN"
+    for next_word in range (noun_index+1, len(sentence)):
+        #starting from the next index, get pos
+        word = sentence[next_word]
+        text = [word]
+        #must call pos_tag on a list, otherwise it will pos_tag each letter
+        word_and_pos = pos_tag(text)
+        current_pos = word_and_pos[0][1]
+        if current_pos[0] == "V":
+            #if pos is verb of any tense
+            key = verb.infinitive(word)
+            #convert the verb into infinitive form to insert into dictionary
 
-#hardcoded for now
-noun = "boy"
+            default_temp_dict = dict()
+            default = default_temp_dict
+            #if the noun form has changed and is not in the dictionary.
+            #exception catcher: Atlanta's vs Atlanta
+            temp_dict = all_nouns.setdefault(singular_noun, default)
 
-for sentence in sentence_list:
-    for word in sentence:
-        #if sentence contains noun, add to filtered_list
-        if word == noun:
-            filtered_list.append(sentence)
+            if temp_dict.has_key(key):
+                #add/update its frequency in the dictionary
+                temp_dict[key]+=1
+            else:
+                temp_dict[key]=1
+            all_nouns[singular_noun] = temp_dict
+            break
+            #only get the next verb after the noun, not all verbs in sentence
 
-verb_dictionary = dict()
+def main():
+    #sentence_list = gutenberg.sents('austen-emma.txt')
+    sentence_list = brown.sents('ca01')
 
-for filtered_sentence in filtered_list:
-    for word in range (0, len(filtered_sentence)):
-        if filtered_sentence[word]==noun:
-            #get the index of the noun
-            current_pos = "NN"
-            for next_word in range (word+1, len(filtered_sentence)):
-                #starting from the next index, get pos
-                #key = filtered_sentence[next_word]
-                text = [filtered_sentence[next_word]]
-                #must call pos_tag on a list, otherwise it will pos_tag each letter
-                word_and_pos = pos_tag(text)
-                current_pos = word_and_pos[0][1]
-                if current_pos[0] == "V":
-                    #if pos is verb of any tense
-                    verb_any_form = word_and_pos[0][0]
-                    key = verb.infinitive(verb_any_form)
-                    #convert the verb into infinitive form to insert into dictionary
-                    if verb_dictionary.has_key(key):
-                        #add/update its frequency in the dictionary
-                        verb_dictionary[key]+=1
+    all_nouns = dict()
+
+    for sentence in sentence_list:
+        for i in range(0, len(sentence)): #for word in sentence
+            word = sentence[i]
+            text = [word]
+            #must call pos_tag on a list, otherwise it will pos_tag each letter
+            word_and_pos = pos_tag(text)
+            current_pos = word_and_pos[0][1]
+
+            #all nouns (singular and plural) but not proper
+            if current_pos[0] == "N":
+                if not current_pos[-1] == "P":
+                    singular_noun = noun.singular(word)
+                    #only insert the singular form of verb into dictionary
+                    if all_nouns.has_key(singular_noun):
+                        find_next_verb(i, sentence, singular_noun, all_nouns)
                     else:
-                        verb_dictionary[key]=1
-                    break
-                    #only get the next verb after the noun, not all verbs in sentence
+                        verb_dictionary = dict()
+                        all_nouns[word] = verb_dictionary
+                        find_next_verb(i, sentence, singular_noun, all_nouns)
 
-#sort the dictionary by values into a list
-sorted_verbs = sorted(verb_dictionary, key=verb_dictionary.get)
+    # #sort all_nouns alphabetically into a list
+    # sorted_nouns = sorted(all_nouns, key=all_nouns.get)
+    #
+    # all_nouns_final = dict()
+    #
+    # #put the nouns back into a new alphabetically-ordered dictionary
+    # for i in range (0, len(sorted_nouns)):
+    #     all_nouns_final[sorted_nouns[i]] = all_nouns[sorted_nouns[i]]
 
-for i in range (0, len(sorted_verbs)):
-    print(sorted_verbs[i])
-    #when you print the element, they are normal strings w/o u prefix
+    #sort each dictionary in all_nouns by frequency
+    for word in all_nouns.keys():
+        #for verb in all_nouns[word].keys():
+        verbs = all_nouns[word].keys()
+        num_verbs = len(all_nouns[word].keys())
+        for x in range (0, num_verbs):
+            #sorted_verbs = sorted(all_nouns[word])#, key = all_nouns[word][verbs[x]]) ERROR: INT OBJECT NOT CALLABLE.
+            #sorted_verbs = sorted(all_nouns[word], key = all_nouns[word].get(verbs[x])) ERROR: INT OBJECT NOT CALLABLE.
+            ordered_dict = dict()
+
+            #put the values back into a new dictionary, put it back into all_nouns
+            # for i in range (0, len(sorted_verbs)):
+            #     ordered_dict[sorted_verbs[i]] = all_nouns[word][sorted_verbs[i]]
+
+    #write dictionary to json file
+    with open('data.json', 'w') as outfile:
+        json.dump(all_nouns, outfile)
+
+if __name__ == "__main__":
+    main()
