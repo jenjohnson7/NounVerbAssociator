bd9eee3bef85198591ea8cd0878edd44825cbfe8
Jen <jen.johnson7@icloud.com>
2017-03-11 22:12:09 -0500

    sorted dicts by freq

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ from nltk.corpus import brown
 #from nltk.corpus import gutenberg
 from nltk import sent_tokenize
 from nltk import pos_tag
+import operator
+from collections import OrderedDict
 
 def find_next_verb(noun_index, sentence, singular_noun, all_nouns):
     current_pos = "NN"
@@ -17,8 +19,9 @@ def find_next_verb(noun_index, sentence, singular_noun, all_nouns):
         current_pos = word_and_pos[0][1]
         if current_pos[0] == "V":
             #if pos is verb of any tense
-            key = verb.infinitive(word)
-            #convert the verb into infinitive form to insert into dictionary
+            lower_cased_verb = word.lower()
+            key = verb.infinitive(lower_cased_verb)
+            #convert the verb into infinitive and lower cased form to insert into dictionary
 
             default_temp_dict = dict()
             default = default_temp_dict
@@ -52,8 +55,10 @@ def main():
             #all nouns (singular and plural) but not proper
             if current_pos[0] == "N":
                 if not current_pos[-1] == "P":
-                    singular_noun = noun.singular(word)
-                    #only insert the singular form of verb into dictionary
+                    lower_cased_noun = word.lower()
+                    singular_noun = noun.singular(lower_cased_noun)
+                    #only insert the singular and lower-cased form of verb into dictionary
+                    #WHY ISN"T THIS WORKING?
                     if all_nouns.has_key(singular_noun):
                         find_next_verb(i, sentence, singular_noun, all_nouns)
                     else:
@@ -61,28 +66,26 @@ def main():
                         all_nouns[word] = verb_dictionary
                         find_next_verb(i, sentence, singular_noun, all_nouns)
 
-    # #sort all_nouns alphabetically into a list
-    # sorted_nouns = sorted(all_nouns, key=all_nouns.get)
-    #
-    # all_nouns_final = dict()
-    #
-    # #put the nouns back into a new alphabetically-ordered dictionary
-    # for i in range (0, len(sorted_nouns)):
-    #     all_nouns_final[sorted_nouns[i]] = all_nouns[sorted_nouns[i]]
-
     #sort each dictionary in all_nouns by frequency
     for word in all_nouns.keys():
-        #for verb in all_nouns[word].keys():
-        verbs = all_nouns[word].keys()
-        num_verbs = len(all_nouns[word].keys())
-        for x in range (0, num_verbs):
-            #sorted_verbs = sorted(all_nouns[word])#, key = all_nouns[word][verbs[x]]) ERROR: INT OBJECT NOT CALLABLE.
-            #sorted_verbs = sorted(all_nouns[word], key = all_nouns[word].get(verbs[x])) ERROR: INT OBJECT NOT CALLABLE.
-            ordered_dict = dict()
+        if len(all_nouns[word])>1:
 
-            #put the values back into a new dictionary, put it back into all_nouns
-            # for i in range (0, len(sorted_verbs)):
-            #     ordered_dict[sorted_verbs[i]] = all_nouns[word][sorted_verbs[i]]
+            #sort by decreasing values
+            sorted_by_values= sorted(all_nouns[word].items(), key = operator.itemgetter(1), reverse = True)
+
+            #remembers the order that items are inserted
+            sorted_dict = OrderedDict()
+
+            #reordering the entries in a temp dictionary
+            for i in range (0, len(sorted_by_values)):
+                sorted_dict[sorted_by_values[i][0]] = sorted_by_values[i][1]
+
+            #update the real dictionary
+            all_nouns[word] = sorted_dict
+
+    #sort all_nouns alphabetically into a list
+    #if this will improve query time, do it.
+    #else, do not do this.
 
     #write dictionary to json file
     with open('data.json', 'w') as outfile:
