37b25c094ef359792ef9f487630df10d1e63b574
Jen Johnson <jen@jens-mbp.middlebury.edu>
2017-04-09 22:21:49 -0400

    ran files on brown corpus for measuring results

--- createjson.py
+++ createjson.py
@@ -4,7 +4,7 @@ import string
 
 import tokenizer_test
 # from nltk.corpus import brown
-# from nltk.corpus import gutenberg
+from nltk.corpus import gutenberg
 # from nltk.corpus import gutenberg
 from nltk import sent_tokenize
 from nltk import pos_tag
@@ -313,11 +313,11 @@ def main():
     '''
 
     #corpus_sents = brown.tagged_sents(categories=['mystery','romance','ficiton','adventure','lore','science_fiction','religion','humor'])
-    corpus_sents2 = tokenizer_test.read_from_file('document.txt')
+    #corpus_sents2 = tokenizer_test.read_from_file('document.txt')
     # corpus_sents3 = tokenizer_test.read_from_file('textdatafolder/1661.txt')
 
-    my_dict_jen = {}
-    my_dict_jen = insert_corpus_v2(my_dict_jen, corpus_sents2)
+    # my_dict_jen = {}
+    # my_dict_jen = insert_corpus_v2(my_dict_jen, corpus_sents2)
 
     # my_dict_v1 = {}
     # my_dict_v2 = {}
@@ -331,7 +331,7 @@ def main():
     #my_dict_v4 = insert_corpus_v4(my_dict_v4, corpus_sents)
 
     # data_dict = {}
-    # data_dict = insert_corpus_v4(data_dict, corpus_sents3)
+    # data_dict = insert_corpus_v5(data_dict, corpus_sents)
 
     # my_dict_v5 = insert_corpus_v5(my_dict_v5, corpus_sents)
 
@@ -362,8 +362,11 @@ def main():
     # with open('datav5.json', 'w') as outfile:
     #     json.dump(my_dict_v5, outfile)
 
-    with open('moose.json','w') as outfile:
-        json.dump(my_dict_jen, outfile)
+    # with open('moose.json','w') as outfile:
+    #     json.dump(my_dict_jen, outfile)
+
+    # with open('defaultgutenbergv1.json', 'w') as outfile:
+    #     json.dump(data_dict, outfile)
 
     # with open('textdatafolder/1661.json','w') as outfile:
     #     json.dump(data_dict, outfile)
